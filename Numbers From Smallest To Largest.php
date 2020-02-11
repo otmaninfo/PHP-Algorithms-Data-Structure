a1ad15bc1aacdafacbcbefd9015c9bd897a51e79
Othmane Marzouq <49825005+otmaninfo@users.noreply.github.com>
2020-02-12 00:09:54 +0100

    Create Numbers From Smallest To Largest.php

---
+++ Numbers From Smallest To Largest.php
@@ -0,0 +1,26 @@
+<?php
+
+//this algorithm for arranging numbers of an array from smallest to largest value.
+//By Othmane Marzouq
+//otmaninfo@gmail.com
+
+//example of data numbers 
+$numbers = array(512, 160, 360, 210, 480, 137, 654);
+
+
+$count = count($numbers);
+$max = max($numbers);
+$result = array();
+
+for ($x = 0; $x < $count; $x++){
+	$min = min($numbers);
+	$result[$x] =  $min;
+	$key = array_search($min, $numbers);
+	unset($numbers[$key]);
+	echo $result[$x] . "<br>";
+}
+
+
+
+
+?>
