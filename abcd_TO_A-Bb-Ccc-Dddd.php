9782b3510c9811a61897dbdcf3e8be48903f51dd
Othmane Marzouq <49825005+otmaninfo@users.noreply.github.com>
2020-04-06 12:57:46 +0100

    Add files via upload

---
+++ abcd_TO_A-Bb-Ccc-Dddd.php
@@ -0,0 +1,32 @@
+<?php
+
+// This function duplicate the character by the number of his position in the string
+
+$text = "abcd"; // Output : A-Bb-Ccc-Dddd
+
+function find($text) {
+ 
+  $array = str_split($text);
+  $count =  count($array);
+
+  for ($x = 0; $x < $count; $x++){
+        for ($n = 0; $n <= $x; $n++){
+            if ($n == 0){
+                echo strtoupper($array[$x]);
+                
+            }
+            else{
+
+                echo strtolower($array[$x]);
+            }
+           
+       } 
+    if ($x + 1 != $count){
+        echo "-";
+    } 
+    
+  }
+
+}
+
+?>
